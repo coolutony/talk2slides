59487aa9376c592e5e06f2c8259445007e554c35
shstan <shstan@gmail.com>
2020-02-13 23:21:51 +0900

    added CSR part

--- record/views.py
+++ record/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.template import RequestContext
 import base64
+import naver_api_utils as naive
+import configparser
 
+config = configparser.ConfigParser()
+config.read('config.ini')
 
 def index(request):
     #return HttpResponse("Hello, world. You're at the polls index.")
@@ -13,4 +17,7 @@ def receive(request):
     context_instance=RequestContext(request)
     print(request.FILES.get('data'))
     data = request.FILES.get('data') #This data is blob
-    return JsonResponse({"msg":"Awesome"}) #TODO: appropriate response
+    text = naive.parse_audio(config['NAVER_AI_API']['client_id'],\
+                      config['NAVER_AI_API']['client_secret'],\
+                      audio_bytes=data, lang="Kor")
+    return JsonResponse({"msg":text}) #TODO: appropriate response
